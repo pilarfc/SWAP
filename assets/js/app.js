3fc3736d0397d7d7af9db7d34b2237dbd52f3445
MemeCast <memecast@MacBook-de-Jonathan.local>
2017-06-12 10:14:01 -0500

    segunda version

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,22 @@
 var cargarPagina = function (){
   cargarPersonajes();
+
 };
 
 
 var cargarPersonajes = function () {
-    //Qué hace el metodo JQUERY --> AJAX ??
-    //Realiza una peticion http asincrona.
-    // $.ajax(url,[settings]);
-    // Los settings son opcionales, son opcionales
-    $.ajax('https://swapi.co/api/people/',{
-      // url: 'http://swapi.co/api/people/',
-      method: 'GET',
-      dataType: 'json',
-      success: function (response) {
-        // console.log("respuesta",response);
-        var personajes = response.results;
-        var total = response.count;
-        mostrarPersonajes(personajes);
-        mostrarTotalPersonajes(total);
-      },
-      error: function (error) {
-        console.log("error",error);
-      }
-    });
+
+var url = 'http://swapi.co/api/people/';
+$.get (url, function (response) {
+  console.log(response);
+
+  var personajes = response.results;
+  var total = response.count;
+
+  mostrarTotalPersonajes(total);
+  mostrarPersonajes(personajes);
+});
+
 };
 var mostrarTotalPersonajes = function (total){
   $("#totalPersonajes").text("total de personajes:"+total);
